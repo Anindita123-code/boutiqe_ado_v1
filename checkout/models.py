d43885b9eb8c273a9cbabcb2cac666ccce7169ae
Anindita Som <aninditasom2020@gmail.com>
2021-04-13 12:55:13 +0000

    forms and signals part 1 code

--- checkout/models.py
+++ checkout/models.py
@@ -41,6 +41,7 @@ class Order(models.Model):
             self.order_number = self._generate_order_number()
         super().save(*args, **kwargs)
 
+
 class OrderLineItem(models.Model):
     order = models.ForeignKey(Order, null=False, blank=False, on_delete=models.CASCADE, related_name='lineitems')
     product = models.ForeignKey(Products, null=False, blank=False, on_delete=models.CASCADE)
@@ -54,4 +55,4 @@ class OrderLineItem(models.Model):
 
     def __str__(self):
         return f'SKU {self.product.sku} on order {self.order.order_number}'
-    
+        
